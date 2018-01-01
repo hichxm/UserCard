7fd045096c12814b9abd20fa7fc6219fcf9c1b89
Unknown <volca780@gmail.com>
2018-01-01 14:28:58 +0100

    Function: install
    Cette fonction installera le plugin avec ces parametre.

--- inc/plugins/usercard.php
+++ inc/plugins/usercard.php
@@ -15,3 +15,40 @@ function usercard_info()
         "compatibility" => "18*"
     ];
 }
+
+/**
+ * @function Plugin installation
+ * @return mixed
+ */
+function usercard_install()
+{
+    global $db;
+
+    $gid = $db->insert_query("settinggroups", [
+        "name"          => "usercard_sg",
+        "title"         => "UserCard",
+        "description"   => "Configuration of UserCard",
+        "disporder"     => 1,
+        "isdefault"     => 0
+    ]);
+
+    $settings = [
+        "usercard_enabled"         => [
+            "title"         => "Enabled",
+            "description"   => "If the yes box is checked, the plugin will be activated.",
+            "optionscode"   => "yesno",
+            "value"         => 1,
+            "disporder"     => 1
+        ]
+    ];
+
+    foreach ($settings as $name => $setting) {
+        $setting["name"]    = $name;
+        $setting["gid"]     = $gid;
+
+        $db->insert_query("settings", $setting);
+    }
+
+    rebuild_settings();
+}
+
