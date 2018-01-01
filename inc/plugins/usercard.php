37fcbd99fdb0d299659fb7e02a3b05ae2f192f46
Unknown <volca780@gmail.com>
2018-01-01 14:25:58 +0100

    Function: info
    Retourne les informations du plugin.

---
+++ inc/plugins/usercard.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @function Return plugin information
+ * @return array
+ */
+function usercard_info()
+{
+    return [
+        "name"          => "UserCard",
+        "description"   => "Display user card.",
+        "author"        => "volca780",
+        "authorsite"    => "https://github.com/volca780",
+        "version"       => "1.0",
+        "compatibility" => "18*"
+    ];
+}
